823543de34465e89d3da64584a379c5e7e40fd5e
Unknown <guilllaumeaxel@gmail.com>
2016-11-20 17:55:24 +0100

    Suprssion WORMS

--- worms/models/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	
-	$bdd = new PDO('mysql:host=localhost;dbname=basemmi14d06','mmi14d06','FrDP');
-
-?>
--- worms/models/getHightScore.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	require 'connect.php';
-
-	$req = $bdd->query('SELECT id,score,pseudo FROM worms_score ORDER BY score DESC LIMIT 10');
-	$res = $req->fetchAll();
-
-	echo json_encode($res);
-?>
